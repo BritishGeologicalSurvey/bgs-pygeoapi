49ccbfa1bdb3f68f3aaa67cc259e44c89ffeeb1a
Edd <edlew@bgs.ac.uk>
2021-06-02 15:46:16 +0100

    revert to hello world

--- content/process/ags_validator.py
+++ content/process/ags_validator.py
@@ -37,8 +37,8 @@ LOGGER = logging.getLogger(__name__)
 #: Process metadata and description
 PROCESS_METADATA = {
     'version': '0.2.0',
-    'id': 'ags-validator',
-    'title': 'AGS Validator',
+    'id': 'hello-world',
+    'title': 'Hello World',
     'description': 'An example process that takes a name as input, and echoes'
                    'it back as output. Intended to demonstrate a simple'
                    'process with a single literal input.',
